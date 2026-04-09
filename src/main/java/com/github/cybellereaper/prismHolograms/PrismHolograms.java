edaf8da913a20349b98aae39f6809f8c9dfedb2a
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-04-09 09:46:33 -0500

    init

---
+++ src/main/java/com/github/cybellereaper/prismHolograms/PrismHolograms.java
@@ -0,0 +1,16 @@
+package com.github.cybellereaper.prismHolograms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrismHolograms extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
